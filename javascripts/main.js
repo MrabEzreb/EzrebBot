13946b86765c79ca56b09760c4434ef646ff3237
MrabEzreb <mrabezreb@outlook.com>
2015-09-16 09:31:27 -0400

    :D

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,6 @@
-console.log('This would be the main JS file.');
+function gotohome() {
+    window.location = "index.html";
+}
+window.onload = function() {
+    document.getElementsByClassName("title")[0] .onclick = gotohome;
+}
